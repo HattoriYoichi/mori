ca6fe6c2d8f505ea831a09f18d38848c7a86a63c
HattoriYoichi <43986315+HattoriYoichi@users.noreply.github.com>
2018-10-09 19:33:39 +0900

    create project
    first commit

---
+++ Sample/Assets/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int hp = 0;
+        if (hp == 0)
+        {
+            Debug.Log("ただの屍のようだ");
+        }
+        else if(hp <= 50)
+        {
+            Debug.Log("先に行け…");
+        }
+        else if(hp >= 200)
+        {
+            Debug.Log("攻!");
+        }
+        else
+        {
+            Debug.Log("防!");
+        }
+
+        }
+}
